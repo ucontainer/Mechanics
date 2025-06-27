c7d1340fcedab759b2194cda2bab6ce78ec32e36
ucontainer <sylunet@gmail.com>
2025-06-26 20:54:49 -0500

    second commit

--- app/blueprints/service_tickets/ticketSchema.py
+++ app/blueprints/service_tickets/ticketSchema.py
@@ -5,5 +5,6 @@ from app.models import Invoice
 class TicketSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = Invoice
+        include_fk = True
 ticket_schema = TicketSchema()  #used to serialize a single customer object.
 tickets_schema = TicketSchema(many=True)  #used to serialize many customer objects
